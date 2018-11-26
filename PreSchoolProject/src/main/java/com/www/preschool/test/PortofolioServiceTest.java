20a2fbc546437be3ca23711debdd2abe7bdb0fbc
donghyeonmin <donghyeonmin@172.19.108.137>
2018-11-26 21:27:48 +0900

    포토폴리오Test 다듬

--- PreSchoolProject/src/main/java/com/www/preschool/test/PortofolioServiceTest.java
+++ PreSchoolProject/src/main/java/com/www/preschool/test/PortofolioServiceTest.java
@@ -1,8 +1,11 @@
 package com.www.preschool.test;
 
+
+import org.junit.Before;
 import org.junit.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 
+import com.www.preschool.dto.PortfolioDto;
 import com.www.preschool.service.PortofolioService;
 
 public class PortofolioServiceTest {
@@ -10,6 +13,14 @@ public class PortofolioServiceTest {
 	@Autowired
 	PortofolioService portofolioService;
 	
+	PortfolioDto portfolioDto = null;
+	
+	// 테스트 실행하기전에 넣어야할 작업관련 메소드
+	@Before
+	public void setUp() {
+		
+	
+	}
 	
 	// DB에 넣고 가져오기 테스트  
 	@Test
