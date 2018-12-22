d74f1c4ad300fb8b386ff248572cfb396cacc939
DonghyeonMin <kei890@naver.com>
2018-12-22 10:19:45 +0900

    병합 작업

--- PreSchoolProject/src/main/java/com/www/preschool/controller/PortfolioController.java
+++ PreSchoolProject/src/main/java/com/www/preschool/controller/PortfolioController.java
@@ -3,7 +3,11 @@ package com.www.preschool.controller;
 import java.util.HashMap;
 import java.util.Map;
 
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -11,6 +15,8 @@ import com.www.preschool.service.PortfolioService;
 
 @RestController
 public class PortfolioController {
+	
+	final static String ADD_ERROR = "300001";
 	
 	@Autowired
 	PortfolioService portfolioService;
@@ -25,9 +31,41 @@ public class PortfolioController {
 		
 	}
 	
-	@RequestMapping("/folio/add")
-	public Map<String, Object> addPortfolio() {
+	@RequestMapping("/portfolio/add")
+	public Map<String, Object> addPortfolio(@RequestBody Map<String, Object> paramMap,
+			HttpServletRequest request,HttpServletResponse response) {
+		
+		System.out.println(paramMap.toString());
+		
+		Map<String,Object> resultMap = new HashMap<>();
+		
+		resultMap.put("code", ADD_ERROR);
+		resultMap.put("message", "등록에 실패했습니다.");
+		
+		
+		return resultMap;
+	}
+	
+	@RequestMapping("/portfolio/update")
+	public Map<String, Object> updatePortfolio(@RequestBody Map<String, Object> paramMap,
+			HttpServletRequest request,HttpServletResponse response) {
+			Map<String, Object> resultMap = new HashMap<>();
+				return paramMap;
+		
+	}
+	
+	@RequestMapping("/portfolio/delete")
+	public Map<String, Object> deletePortfolio(@RequestBody Map<String, Object> paramMap,
+			HttpServletRequest request,HttpServletResponse response) {
+		Map<String, Object> resultMap = new HashMap<>();
+		
 		
 		return null;
 	}
+	
+//	@RequestMapping("/portfolio/add")
+//	public Map<String, Object> addPortfolio(@RequestBody Map<String, Object> paramMap,
+//			HttpServletRequest request,HttpServletResponse response) {
+//		
+//	}
 }
