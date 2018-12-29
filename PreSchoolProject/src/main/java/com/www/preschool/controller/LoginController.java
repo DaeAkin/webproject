53c64c6c3241f2fbd1766d51d99712b4cc24e1fd
wjsal <wjsal@DESKTOP-P6QO13Q>
2018-12-29 23:02:41 +0900

    Mybatis 적용 완료. LoginController, mybatis parametertype/resulttype 미완성.

--- PreSchoolProject/src/main/java/com/www/preschool/controller/LoginController.java
+++ PreSchoolProject/src/main/java/com/www/preschool/controller/LoginController.java
@@ -1,12 +1,16 @@
 package com.www.preschool.controller;
 
+import java.util.Map;
+
 import javax.annotation.Resource;
 import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
 import javax.servlet.http.HttpSession;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.ResponseBody;
@@ -14,6 +18,7 @@ import org.springframework.web.bind.annotation.RestController;
 
 import com.www.preschool.dto.MemberDto;
 import com.www.preschool.service.LoginService;
+import com.www.preschool.utils.JWTUtil;
 
 
 
@@ -25,20 +30,26 @@ public class LoginController {
 	@Resource
 	HttpSession session;
 
-	@RequestMapping(value = "/login", method = RequestMethod.POST)
-	public int login(MemberDto member, HttpServletRequest request) {
+	@RequestMapping(value = "/admin/login", method = RequestMethod.POST)
+	public int login(@RequestBody Map<String, Object> paramMap, HttpServletRequest request, HttpServletResponse response) {
+		
+		System.out.println("connected");
+		response.setHeader("token", 
+				JWTUtil.createToken((String)paramMap.get("testId")));
+		
 		if(session.getAttribute("session_prs")!=null) {
 			session.removeAttribute("session_prs");
 		}
 
-		MemberDto session_prs = loginService.login(member);
+		//
+		//MemberDto session_prs = loginService.login(member);
 
-		if (session_prs != null) {
-			session.setAttribute("session_prs", session_prs);
-			return 1;
-		} else {
-			return 0;
-		}
+//		if (session_prs != null) {
+//			session.setAttribute("session_prs", session_prs);
+//			return 1;
+//		} else {
+//			return 0;
+//		}
 		
 	}
 	
